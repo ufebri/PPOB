4291afdb8393f4fd129ec0e47fad580b6341a51e
Uray Febri Isviladinata <ray@Urays-MacBook-Air.local>
2023-12-26 10:07:54 +0700

    init: migration

--- app/src/main/java/febri/uray/bedboy/tryingkotlin/RecyclerView/ItemFootballClub.kt
+++
@@ -1,7 +0,0 @@
-package febri.uray.bedboy.tryingkotlin.RecyclerView
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class ItemFootballClub(val name: String?, val image: Int?) : Parcelable
