56c102ad64389e430ba133b40590eb45cffe5d05
Uray Febri Isviladinata <ray@Urays-MacBook-Air.local>
2024-01-21 20:21:42 +0700

    add: color when typing search

--- app/src/main/java/febri/uray/bedboy/ppob/presentations/home/HomeFragment.kt
+++ app/src/main/java/febri/uray/bedboy/ppob/presentations/home/HomeFragment.kt
@@ -1,10 +1,12 @@
 package febri.uray.bedboy.ppob.presentations.home
 
+import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import androidx.core.widget.doAfterTextChanged
